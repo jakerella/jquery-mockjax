9a99183661712db7c735b15bad78baca255ffc98
jakerella <jordan@jordankasper.com>
2014-08-13 12:43:02 -0500

    Added fix for #131 by changing request headers to be placed on requestSettings object instead of mockHandler, which is, essentially, the response settings

--- jquery.mockjax.js
+++ jquery.mockjax.js
@@ -248,7 +248,8 @@
 				clearTimeout(this.responseTimer);
 			},
 			setRequestHeader: function(header, value) {
-				mockHandler.headers[header] = value;
+				requestSettings.headers[header] = value;
+				// mockHandler.headers[header] = value;
 			},
 			getResponseHeader: function(header) {
 				// 'Last-modified', 'Etag', 'content-type' are all checked by jQuery
