6ceb4f8a3818d74bcce339df3a22ecd1754edd36
jakerella <jordan@jordankasper.com>
2014-11-23 18:36:02 -0500

    Added automated running of all supported jQuery versions through Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,11 +2,27 @@
 module.exports = function(grunt) {
 	'use strict';
 
+	var jQueryVersions = [
+		'1.5.2',
+		'1.6.4',
+		'1.7.2',
+		'1.8.3',
+		'1.9.1',
+		'1.10.2',
+		'1.11.1',
+		'2.0.3',
+		'2.1.1',
+		'git',
+	];
+		/*
+		<a href="?jquery=1.9.1">jQuery 1.9.1</a>
+		 */
+
 	// Project configuration.
 	grunt.initConfig({
 		// Metadata.
 		pkg: grunt.file.readJSON('package.json'),
-		
+
 		banner: '/*! MockJax - jQuery Plugin to Mock Ajax requests \n'+
 			'* <%= pkg.title || pkg.name %>\n' +
 			'* \n' +
@@ -48,8 +64,33 @@ module.exports = function(grunt) {
 				src: 'src/**/*.js'
 			}
 		},
-		qunit: {
-			all: ['test/index.html']
+		qunit: { all: [] },  // NOTE: these tests are all run by the `test` task below to run against each jQuery version supported
+		test: {
+			jQueryVersions: [
+				'1.5.2',
+				'1.6.4',
+				'1.7.2',
+				'1.8.3',
+				'1.9.1',
+				'1.10.2',
+				'1.11.1',
+				'2.0.3',
+				'2.1.1',
+				'git'
+			],
+			latestInBranch: {
+				jQueryVersions: [
+					'1.11.1',
+					'2.1.1'
+				]
+			},
+			oldestAndLatest: {
+				jQueryVersions: [
+					'1.5.2',
+					'1.11.1',
+					'2.1.1'
+				]
+			}
 		},
 		watch: {
 			gruntfile: {
@@ -67,5 +108,20 @@ module.exports = function(grunt) {
 
 	grunt.registerTask('build', ['concat', 'uglify']);
 	grunt.registerTask('default', ['jshint', 'qunit', 'build']);
+
+
+	grunt.registerTask('test', 'Executes QUnit tests with all supported jQuery versions', function() {
+		var i, l,
+			versionUrls = [],
+			versions = grunt.config.get('test' + ((arguments[0]) ? '.'+arguments[0] : '') + '.jQueryVersions') || [];
+
+		for (i=0, l=versions.length; i<l; ++i) {
+			grunt.log.writeln('Adding jQuery version to test: ' + versions[i]);
+			versionUrls.push('test/index.html?jquery=' + versions[i]);
+		}
+
+		grunt.config.set('qunit.options.urls', versionUrls);
+		grunt.task.run('qunit');
+	});
 
 };
