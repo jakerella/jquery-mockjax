82376e8bc86dfb45325218537737379909f077d4
jakerella <jordan@jordankasper.com>
2014-11-23 17:49:05 -0500

    Switched Gruntfile to using tabs vs spaces to be consistent

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,69 +2,70 @@
 module.exports = function(grunt) {
 	'use strict';
 
-    // Project configuration.
-    grunt.initConfig({
-        // Metadata.
-        pkg: grunt.file.readJSON('package.json'),
-        banner: '/*! MockJax - jQuery Plugin to Mock Ajax requests \n'+
-            '* <%= pkg.title || pkg.name %>\n' +
-            '* \n' +
-            '* Version: <%= pkg.version %> \n' +
-            '* Released: <%= grunt.template.today("yyyy-mm-dd") %> \n' +
-            '* Home: <%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
-            '* Copyright (c) <%= grunt.template.today("yyyy") %> Jordan Kasper, formerly appendTo;\n' +
-            '* NOTE: This repository was taken over by Jordan Kasper (@jakerella) October, 2014\n' +
-            '* \n' +
-            '* Dual licensed under the MIT or GPL licenses.\n' +
-            '* http://opensource.org/licenses/MIT OR http://www.gnu.org/licenses/gpl-2.0.html\n' +
-            '*/\n',
-            
-        // Task configuration.
-        concat: {
-            options: {
-                banner: '<%= banner %>',
-                stripBanners: true
-            },
-            dist: {
-                src: ['src/<%= pkg.name %>.js'],
-                dest: 'dist/<%= pkg.name %>.js'
-            }
-        },
-        uglify: {
-            options: {
-                banner: '<%= banner %>'
-            },
-            dist: {
-                src: '<%= concat.dist.dest %>',
-                dest: 'dist/<%= pkg.name %>.min.js'
-            }
-        },
-        jshint: {
-            options: {
-                jshintrc: true
-            },
-            all: {
-                src: 'src/**/*.js'
-            }
-        },
-        qunit: {
-            all: ['test/index.html']
-        },
-        watch: {
-            gruntfile: {
-                files: '<%= jshint.gruntfile.src %>',
-                tasks: ['jshint:gruntfile']
-            },
-            lib_test: {
-                files: '<%= jshint.lib_test.src %>',
-                tasks: ['jshint:lib_test', 'nodeunit']
-            }
-        }
-    });
+	// Project configuration.
+	grunt.initConfig({
+		// Metadata.
+		pkg: grunt.file.readJSON('package.json'),
+		
+		banner: '/*! MockJax - jQuery Plugin to Mock Ajax requests \n'+
+			'* <%= pkg.title || pkg.name %>\n' +
+			'* \n' +
+			'* Version: <%= pkg.version %> \n' +
+			'* Released: <%= grunt.template.today("yyyy-mm-dd") %> \n' +
+			'* Home: <%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+			'* Copyright (c) <%= grunt.template.today("yyyy") %> Jordan Kasper, formerly appendTo;\n' +
+			'* NOTE: This repository was taken over by Jordan Kasper (@jakerella) October, 2014\n' +
+			'* \n' +
+			'* Dual licensed under the MIT or GPL licenses.\n' +
+			'* http://opensource.org/licenses/MIT OR http://www.gnu.org/licenses/gpl-2.0.html\n' +
+			'*/\n',
+			
+		// Task configuration.
+		concat: {
+			options: {
+				banner: '<%= banner %>',
+				stripBanners: true
+			},
+			dist: {
+				src: ['src/<%= pkg.name %>.js'],
+				dest: 'dist/<%= pkg.name %>.js'
+			}
+		},
+		uglify: {
+			options: {
+				banner: '<%= banner %>'
+			},
+			dist: {
+				src: '<%= concat.dist.dest %>',
+				dest: 'dist/<%= pkg.name %>.min.js'
+			}
+		},
+		jshint: {
+			options: {
+				jshintrc: true
+			},
+			all: {
+				src: 'src/**/*.js'
+			}
+		},
+		qunit: {
+			all: ['test/index.html']
+		},
+		watch: {
+			gruntfile: {
+				files: '<%= jshint.gruntfile.src %>',
+				tasks: ['jshint:gruntfile']
+			},
+			lib_test: {
+				files: '<%= jshint.lib_test.src %>',
+				tasks: ['jshint:lib_test', 'nodeunit']
+			}
+		}
+	});
 
-    require('load-grunt-tasks')(grunt);
+	require('load-grunt-tasks')(grunt);
 
-    grunt.registerTask('build', ['concat', 'uglify']);
-    grunt.registerTask('default', ['jshint', 'qunit', 'build']);
+	grunt.registerTask('build', ['concat', 'uglify']);
+	grunt.registerTask('default', ['jshint', 'qunit', 'build']);
 
 };
