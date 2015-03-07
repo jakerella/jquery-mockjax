893d02a0d4508a32d0864d0aca62f95348611579
Ferdinand Prantl <prantlf@gmail.com>
2015-03-07 21:17:15 +0100

    Add wrapper for AMDJS, CommonJS and browser module
    Use the pattern from https://github.com/umdjs/umd
    Use 'jquery' module dependency for jQuery.

--- src/jquery.mockjax.js
+++ src/jquery.mockjax.js
@@ -1,4 +1,19 @@
-(function($) {
+(function(root, factory) {
+	'use strict';
+
+	// AMDJS module definition
+	if ( typeof define === 'function' && define.amd ) {
+		define(['jquery'], function($) {
+			return factory($);
+		});
+	// CommonJS module definition
+	} else if ( typeof exports === 'object') {
+		module.exports = factory(require('jquery'));
+	// Global jQuery in web browsers
+	} else {
+		return factory(root.jQuery || root.$);
+	}
+}(this, function($) {
 	'use strict';
 
 	var _ajax = $.ajax,
@@ -710,4 +725,7 @@
 	$.mockjax.unmockedAjaxCalls = function() {
 		return unmockedAjaxCalls;
 	};
-})(jQuery);
+
+	return $.mockjax;
+
+}));
