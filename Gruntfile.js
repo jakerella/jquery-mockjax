efebd450249b47c5bec3cb3c098ac90eb6e57d2f
jakerella <jordan@jordankasper.com>
2015-04-08 11:41:04 -0400

    Updated distribution banner: removed 'release' date

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,20 +5,21 @@ module.exports = function(grunt) {
     grunt.initConfig({
         // Metadata.
         pkg: grunt.file.readJSON('package.json'),
-        banner: '/*! MockJax - jQuery Plugin to Mock Ajax requests \n'+
-            '* <%= pkg.title || pkg.name %>\n' +
-            '* \n' +
-            '* Version: <%= pkg.version %> \n' +
-            '* Released: <%= grunt.template.today("yyyy-mm-dd") %> \n' +
-            '* Home: <%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
-            '* Copyright (c) <%= grunt.template.today("yyyy") %> Jordan Kasper, formerly appendTo;\n' +
-            '* NOTE: This repository was taken over by Jordan Kasper (@jakerella) October, 2014\n' +
-            '* \n' +
-            '* Dual licensed under the MIT or GPL licenses.\n' +
-            '* http://opensource.org/licenses/MIT OR http://www.gnu.org/licenses/gpl-2.0.html\n' +
-            '*/\n',
-            
-        // Task configuration.
+        banner: [
+            '/*! MockJax - jQuery Plugin to Mock Ajax requests',
+            '* <%= pkg.title || pkg.name %>',
+            '* ',
+            '* Version: <%= pkg.version %> ',
+            '* Home: <%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>',
+            '* Copyright (c) <%= grunt.template.today("yyyy") %> Jordan Kasper, formerly appendTo;',
+            '* NOTE: This repository was taken over by Jordan Kasper (@jakerella) October, 2014',
+            '* ',
+            '* Dual licensed under the MIT or GPL licenses.',
+            '* http://opensource.org/licenses/MIT OR http://www.gnu.org/licenses/gpl-2.0.html',
+            '*/\n'
+        ].join('\n'),
+        
+        // Task configuration
         concat: {
             options: {
                 banner: '<%= banner %>',
